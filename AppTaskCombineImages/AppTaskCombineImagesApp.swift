1307dea9709306c01e58c7a0d2c71899684334ed
Volodimer1987 <kynicin25121987@gmail.com>
2024-10-07 11:42:13 +0300

    Initial Commit

---
+++ AppTaskCombineImages/AppTaskCombineImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTaskCombineImagesApp.swift
+//  AppTaskCombineImages
+//
+//  Created by vladimir gennadievich on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTaskCombineImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
